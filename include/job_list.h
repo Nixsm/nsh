b41392652d61d9c67f36b636edda8e7b47ee1fb5
Nixsm <nichsmm@gmail.com>
2015-09-06 18:03:46 +0000

    .

---
+++ include/job_list.h
@@ -0,0 +1,51 @@
+#ifndef NSH_JOB_LIST_H
+#define NSH_JOB_LIST_H
+
+#include <unistd.h>
+#include "config.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+typedef enum {
+    FOREGROUND = 0,
+    BACKGROUND,
+    STOPPED,
+    DONE,
+    TERMINATED
+} job_status_t;
+
+typedef struct job {
+    char* runString; /* String called by the user */
+    pid_t pgid; /* Group id of current proccess */
+    int job; /* job index inside list */
+    job_status_t jStatus; /* Status of this job */
+    struct job* next;
+} job_node_t;
+
+typedef struct {
+    unsigned int size;
+    job_node_t* begin;
+} Jobs;
+
+Jobs createList();
+
+void destroyList(INOUT Jobs* joblist);
+
+int add(INOUT Jobs* jobList, IN pid_t pgid, IN char* cmd, IN job_status_t status);
+
+int delete(INOUT Jobs* jobList, IN int job);
+
+void showJobs(INOUT Jobs* jobList);
+
+int setStatus(INOUT Jobs* jobList, IN int job, IN job_status_t status);
+
+int getStatus(INOUT Jobs* jobList, IN int job, IN job_status_t status);
+
+pid_t getProcess(INOUT Jobs* jobList, IN int job);
+
+int getLargest(INOUT Jobs* jobList);
+
+char* getStatusString(IN job_status_t status);
+
+#endif//NSH_JOB_LIST_H
---
+++ src/job_list.c
@@ -0,0 +1,135 @@
+#include "job_list.h"
+
+
+Jobs createList() {
+    Jobs jobs;
+    jobs.size = 0;
+    jobs.begin = NULL;
+    return jobs;
+}
+
+void destroyList(INOUT Jobs* joblist) {
+    job_node_t* job;
+    while (joblist->begin) {
+        job = joblist->begin;
+        joblist->begin = joblist->begin->next;
+        free(job);
+    }
+
+    joblist->size = 0;
+    joblist->begin = NULL;
+}
+
+int add(INOUT Jobs* jobList, IN pid_t pgid, IN char* cmd, IN job_status_t status) {
+    job_node_t* begin = jobList->begin;
+
+    job_node_t* newNode = (job_node_t*)calloc(1, sizeof(job_node_t));
+    
+    newNode->pgid = pgid;
+    strcpy(newNode->runString, cmd);
+    newNode->job = ++(jobList->size);
+    newNode->jStatus = status;
+    
+    while (begin) {
+        begin = begin->next;
+    }
+
+    begin->next = newNode;
+}
+
+int delete(INOUT Jobs* jobList, IN int job) {
+    job_node_t* begin = jobList->begin;
+
+    job_node_t* before = NULL;
+
+    job_node_t* toBeDeleted = NULL;
+    
+    while (begin) {
+        if (begin->job == job){
+            break;
+        }
+        before = begin;
+        begin = begin->next;
+    }
+
+    if (!before) {
+        free(jobList->begin);
+        jobList->begin = begin->next;
+    } else {
+        toBeDeleted = before->next;
+        
+        before->next = toBeDeleted->next;
+        
+        free(toBeDeleted);
+    }
+
+    jobList->size--;
+}
+
+char* getStatusString(IN job_status_t status) {
+    switch (status) {
+    case FOREGROUND:
+        return "FOREGROUND";
+    case BACKGROUND:
+        return "BACKGROUND";
+    case STOPPED:
+        return "STOPPED";
+    case DONE:
+        return "DONE";
+    case TERMINATED:
+        return "TERMINATED";
+    }
+}
+
+void showJobs(INOUT Jobs* jobList) {
+    job_node_t* begin = jobList->begin;
+
+    printf("[JobID] status groupID CMD\n");
+    while (begin) {
+        printf("[%d] %s %ld %s\n", begin->job, getStatusString(begin->jStatus), begin->pgid, begin->runString);
+        begin = begin->next;
+    }
+}
+
+int setStatus(INOUT Jobs* jobList, IN int job, IN job_status_t status) {
+    job_node_t* begin = jobList->begin;
+
+    while (begin) {
+        if (begin->job == job){
+            break;
+        }
+
+        begin = begin->next;
+    }
+
+    begin->jStatus = status;
+}
+
+int getStatus(INOUT Jobs* jobList, IN int job, IN job_status_t status) { 
+    job_node_t* begin = jobList->begin;
+
+    while (begin) {
+        if (begin->job == job){
+            return begin->jStatus;
+        }
+
+        begin = begin->next;
+    }   
+}
+
+pid_t getProcess(INOUT Jobs* jobList, IN int job) {
+        job_node_t* begin = jobList->begin;
+
+    while (begin) {
+        if (begin->job == job){
+            return begin->pgid;
+        }
+
+        begin = begin->next;
+    }
+
+}
+
+int getLargest(INOUT Jobs* jobList) {
+    
+}
